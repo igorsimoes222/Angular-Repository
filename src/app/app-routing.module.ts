4fc7c3fcdf04eb03804c7f2915d7445ba17f41fa
igorsimoes222 <igorsimoescs2008@gmail.com>
2022-08-15 10:51:57 -0300

    teste commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { LoginComponent } from './pages/login/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho/carrinho.component';
+import { PagamentoComponent } from './pages/pagamento/pagamento/pagamento.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'', component:HomeComponent},
+{path:'menu', component:MenuComponent},
+{path:'about', component:AboutComponent},
+{path:'contact', component:ContactComponent},
+{path:'carrinho', component:CarrinhoComponent},
+{path:'login', component:LoginComponent},
+{path:'pagamento', component:PagamentoComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatBadgeModule} from '@angular/material/badge';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho/carrinho.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { MenuComponent } from './pages/menu/menu.component';
+import { LoginComponent } from './pages/login/login/login.component';
+import { PagamentoComponent } from './pages/pagamento/pagamento/pagamento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    MenuComponent,
+    LoginComponent,
+    PagamentoComponent,
+    CarrinhoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatBadgeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
